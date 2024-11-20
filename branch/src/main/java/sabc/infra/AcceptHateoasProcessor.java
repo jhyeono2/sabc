dcd3b6bf158a6994ec9eadf87ac1bfc5ef249f0a
jhyeono2 hyeonyeong <j.hyeono2@gmail.com>
2024-11-20 03:58:37 +0000

    에러제거

--- branch/src/main/java/sabc/infra/AcceptHateoasProcessor.java
+++ branch/src/main/java/sabc/infra/AcceptHateoasProcessor.java
@@ -1,7 +1,6 @@
 package sabc.infra;
 
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
 import sabc.domain.*;
