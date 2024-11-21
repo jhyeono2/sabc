b8f852153b65be6811424ae544bf31d751400d38
jhyeono2 hyeonyeong <j.hyeono2@gmail.com>
2024-11-21 13:20:20 +0000

    asd

--- gateway/src/main/java/sabc/Application.java
+++ gateway/src/main/java/sabc/Application.java
@@ -11,5 +11,6 @@ public class Application {
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
+        System.out.println("###########gateway start");
     }
 }
