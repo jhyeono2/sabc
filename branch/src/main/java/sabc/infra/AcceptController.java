51692d4cf5b0158706b15b56025b8b8c0ff9b7bf
jhyeono2 hyeonyeong <j.hyeono2@gmail.com>
2024-11-21 13:49:08 +0000

    ㅅㄱㄷㄴㅅ

--- branch/src/main/java/sabc/infra/AcceptController.java
+++ branch/src/main/java/sabc/infra/AcceptController.java
@@ -2,6 +2,7 @@ package sabc.infra;
 
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sabc.domain.*;
 
@@ -14,5 +15,11 @@ public class AcceptController {
 
     @Autowired
     AcceptRepository acceptRepository;
+
+    @GetMapping("/actuator/health")
+    public String health() {
+        // 의도적으로 500 오류 반환
+        throw new RuntimeException("Intentional failure for livenessProbe test");
+    }
 }
 //>>> Clean Arch / Inbound Adaptor
