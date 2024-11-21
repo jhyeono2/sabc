3d6ee2396295a3019759e50051e54b53506e63b2
jhyeono2 hyeonyeong <j.hyeono2@gmail.com>
2024-11-21 06:24:29 +0000

    asdf

--- dashboard/src/main/java/sabc/DashboardApplication.java
+++ dashboard/src/main/java/sabc/DashboardApplication.java
@@ -16,6 +16,7 @@ public class DashboardApplication {
 
     public static void main(String[] args) {
         applicationContext =
-            SpringApplication.run(DashboardApplication.class, args);
+        SpringApplication.run(DashboardApplication.class, args);
+        System.out.println("###########dashboard spring start");
     }
 }
